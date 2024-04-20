71e8b649d0c3a81d1feb1111aea9513c88cd84e4
Arcanyx <nikhilchaughule@gmail.com>
2024-04-20 16:46:48 +0530

    182

---
+++ 182. Duplicate Emails.sql
@@ -0,0 +1,8 @@
+﻿/*
+// https://leetcode.com/problems/duplicate-emails/description/
+
+// 182. Duplicate Emails
+// Difficulty: Easy
+*/
+-- Write your MySQL query statement below
+select email from Person group by email HAVING COUNT(email)>1;
