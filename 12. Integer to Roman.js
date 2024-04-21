3b16a491607a954b87d93d75982d62c1d6c0247d
Arcanyx <nikhilchaughule@gmail.com>
2024-04-21 18:40:11 +0530

    12

---
+++ 12. Integer to Roman.js
@@ -0,0 +1,37 @@
+﻿// https://leetcode.com/problems/integer-to-roman/
+
+// 12. Integer to Roman
+// Difficulty: Medium
+
+// Intuition: Roman numbers are going from left to right
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function (num) {
+  let values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  let symbols = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+  ];
+  let ans = "";
+  for (let i = 0; i < values.length; i++) {
+    // Subtract number till num becomes smaller
+    while (num >= values[i]) {
+      num -= values[i];
+      ans += symbols[i];
+    }
+  }
+  return ans;
+};
