8ee646bd9e5c5476e1474bb36c5f4e0543cab427
Arcanyx <nikhilchaughule@gmail.com>
2024-04-24 22:10:03 +0530

    367

---
+++ 367. Valid Perfect Square.js
@@ -0,0 +1,15 @@
+﻿// https://leetcode.com/problems/valid-perfect-square/description/
+
+// 367. Valid Perfect Square
+// Difficulty: Easy
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function (num) {
+  for (let i = 1; i * i <= num; i++) {
+    if (i * i == num) return true;
+  }
+  return false;
+};
