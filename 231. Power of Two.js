a7cac52db99674f63e276a6f70312ad17e5c7667
Arcanyx <nikhilchaughule@gmail.com>
2024-04-28 16:31:00 +0530

    231

---
+++ 231. Power of Two.js
@@ -0,0 +1,23 @@
+﻿// https://leetcode.com/problems/power-of-two/description/
+
+// 231. Power of Two
+// Difficulty: Easy
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function (n) {
+  // base condition
+  if (n == 1) return true;
+  let i = 0,
+    ans = 1;
+  // as per the contraints -2^31 <= n <= 2^31 - 1 so we have to run this only 31 times
+  while (i < 32 && ans < n) {
+    ans = 1 << i;
+    if (ans == n) return true;
+    i++;
+  }
+
+  return false;
+};
