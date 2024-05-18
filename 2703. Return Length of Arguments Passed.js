67ba8949c300d7fd8460f084009d2c0e427e9d9d
Arcanyx <nikhilchaughule@gmail.com>
2024-05-18 19:24:26 +0530

    2703

---
+++ 2703. Return Length of Arguments Passed.js
@@ -0,0 +1,16 @@
+﻿// https://leetcode.com/problems/return-length-of-arguments-passed/description/
+
+// 2703. Return Length of Arguments Passed
+// Difficulty: Super Easy
+
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function (...args) {
+  return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
