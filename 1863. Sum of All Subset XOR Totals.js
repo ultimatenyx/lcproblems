628fd235e2fe5be87e926239ef64c0bcd7153971
Arcanyx <nikhilchaughule@gmail.com>
2024-05-20 23:03:00 +0530

    1863

---
+++ 1863. Sum of All Subset XOR Totals.js
@@ -0,0 +1,16 @@
+﻿// https://leetcode.com/problems/sum-of-all-subset-xor-totals/description/?envType=daily-question&envId=2024-05-20
+
+// 1863. Sum of All Subset XOR Totals
+// Difficulty: Easy
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var subsetXORSum = function (nums) {
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum |= nums[i];
+  }
+  return sum << (nums.length - 1);
+};
