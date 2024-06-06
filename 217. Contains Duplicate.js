a2ba1fe2018847b5d9e94fe78556b364200911df
Arcanyx <nikhilchaughule@gmail.com>
2024-06-06 09:03:40 +0530

    217

---
+++ 217. Contains Duplicate.js
@@ -0,0 +1,16 @@
+﻿// https://leetcode.com/problems/contains-duplicate/description/
+
+// 217. Contains Duplicate
+// Difficulty: Easy
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  nums.sort();
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] == nums[i - 1]) return true;
+  }
+  return false;
+};
